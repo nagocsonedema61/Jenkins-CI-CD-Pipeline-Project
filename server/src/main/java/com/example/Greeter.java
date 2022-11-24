cef0d62275207092d5f047d36f8de528aeeaa817
moyo Edema <moyogbemiedema13@gmail.com>
2022-11-23 22:01:00 -0600

    testing scan

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,12 +7,14 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
+
   }
+
  /**
    * This is a method.
    */
   public final String greet(final String someone) {
-    
+    String DB_PASSWORD = "Admin@12345";
     return String.format("Hello Avinash, %s!", someone);
   }
 }
